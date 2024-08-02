7689a6262e58169c606de9c87b0ed6bd75950015
fredy-ascuntar <fredy_0793@hotmail.com.com>
2024-08-02 09:21:24 -0500

    configuracion anan

--- Backend/src/model/database.js
+++ Backend/src/model/database.js
@@ -1,20 +1,21 @@
-const mysql = require('mysql')
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from "./config.js";
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'prueba7',
-    multipleStatements: 'true'
-})
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  multipleStatements: "true",
+});
 
 connection.connect(function (err) {
-    if (err) {
-        console.log(err)
-        //return
-    } else {
-        console.log("Connected")
-    }
-})
+  if (err) {
+    console.log(err);
+    //return
+  } else {
+    console.log("Connected");
+  }
+});
 
-module.exports = connection
+module.exports = connection;
